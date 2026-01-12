626467138a94898ae20179d017c5847296b67fa8
ali-b1812 <alib181220@gmail.com>
2026-01-12 20:46:44 +0100

    feat: an enum for userroles for user entity

---
+++ api/src/main/java/com/ali_b1812/app/model/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.ali_b1812.app.model.enums;
+
+public enum UserRole {
+    ADMIN,          // Full access
+    MODERATOR,      // Can moderate content
+    USER,           // Regular user
+    GUEST           // Read-only access
+}
